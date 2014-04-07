344c960e2c061bf3145c99553652d915913f20e5
unknown <CT61557@CT61527PC.na.mmfg.net>
2014-04-07 12:32:41 -0400

    kmp

---
+++ KMP.java
@@ -0,0 +1,48 @@
+package kmp;
+
+public class KMP {
+	
+	public int kmpMatch(String s, String p){
+		int[] next = new int[s.length()];
+		int i,j;
+		i=0;
+		j=0;
+		getNext(p,next);
+		while(i<s.length()){
+			if(j==-1||s.charAt(i)==p.charAt(j)){
+				i++;
+				j++;
+			}else{
+				j=next[j]; // eliminate Trace Back
+			}
+			if(j==p.length()){
+				return i-p.length();
+			}
+		}
+		return -1;
+	}
+	
+	private void getNext(String p, int[] next){
+		int j,k;
+		next[0]=-1;
+		j=0;
+		k=-1;
+		while(j<p.length()-1){
+			if(k==-1 || p.charAt(j)==p.charAt(k)){
+				j++;
+				k++;
+				next[j]=k;
+			}else{
+				k=next[k];
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		KMP instance = new KMP();
+		String s="bacbababaabcbab";
+		String p="ababaca";
+		int index=instance.kmpMatch(s, p);
+		System.out.println(s.substring(index));
+	}
+}
